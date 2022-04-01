7f574892347d5dda34f34dced6d4b1f003985388
Avi <avraham.sikirov@live.biu.ac.il>
2022-04-01 17:11:54 +0300

    adding modal button

---
+++ src/database/Contacts.js
@@ -0,0 +1,12 @@
+const Contacts = [
+    {
+      username: "avi711",
+      userContacts: ["avi", "sudri", "shirin"]
+    },
+    {
+      username: "dan711",
+      userContacts: ["rodin", "shirin", "kakdila"]
+    }
+  ]
+
+export default Contacts;
