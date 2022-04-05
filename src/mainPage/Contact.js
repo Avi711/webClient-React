9e5ec4df58c9cdfd4ecef6cae7bb014ea8adfe9b
Avi <avraham.sikirov@live.biu.ac.il>
2022-04-05 22:10:25 +0300

    toogle color on contacts

--- src/mainPage/Contact.js
+++ src/mainPage/Contact.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
 function Contact(props) {
+
+
+
+    const onContact = function () {
+       //document.getElementsByClassName("toggle-contact").array.foreach((value) => { value.classList.remove("back-top") });
+
+        var slides = document.getElementsByClassName("toggle-contact");
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("toggle-contact-color");
+        }
+        document.getElementById(props.contactName).classList.add("toggle-contact-color")
+    }
+
+    const dontContact = function () {
+        var slides = document.getElementsByClassName("toggle-contact");
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("toggle-contact-color");
+        }
+    }
+
+
+
     return (
-        
-        <tr>
+
+        <tr id={props.contactName} className="toggle-contact" onClick={onContact}>
             <td className="contact-photo-width"><img src={props.image} alt="" className="profile-image" /></td>
             <td><b>{props.contactName} </b><br /> <small>{props.contactMessage}</small></td>
             <td className="time" ><small>{props.time}</small></td>
