c5f332c528e4f894adf78a8f3974f9fc45a92474
Avi <avraham.sikirov@live.biu.ac.il>
2022-04-06 22:27:41 +0300

    adding messege data base

--- src/database/Contacts.js
+++ src/database/Contacts.js
@@ -1,15 +1,21 @@
+
+let avi711Kehat = [{"avi711": "hello", time: "11:47"}, {"kehat": "hello", time: "11:47"}]
+let avi711Or = [{"avi711": "hello", time: "11:47"}, {"kehat": "hello", time: "11:47"}]
+let avi711Rodin = [{"avi711": "hello", time: "11:47"}, {"kehat": "hello", time: "11:47"}]
+
 const Contacts = [
     {
       username: "avi711",
-      userContacts: [{ contactName: 'kehat', contactMessage: 'hello', time: '12:12', image : "profile3.png" },
-  { contactName: 'avi', contactMessage: 'whats up ?', time: '13:09', image : "profile6.png" },
-  { contactName: 'rodin', contactMessage: 'hi bro', time: '14:12' , image : "profile5.png" }]
+      userContacts: [{ contactName: 'kehat', contactMessage: 'hello', time: '12:12', image : "profile3.png", chat:  avi711Kehat},
+  { contactName: 'or', contactMessage: 'whats up ?', time: '13:09', image : "profile6.png", chat:  avi711Or },
+  { contactName: 'rodin', contactMessage: 'hi bro', time: '14:12' , image : "profile5.png", chat:  avi711Rodin }]
     },
     {
       username: "dan711",
       userContacts: [{ contactName: 'sudri', contactMessage: 'hello there', time: '15:12', image : "profile3.png" },
       { contactName: 'shirin', contactMessage: 'whats up ?', time: '13:09', image : "profile5.png" },
       { contactName: 'tal', contactMessage: 'hi bro ehat ap', time: '14:12', image : "profile6.png" }]
+
     }
   ]
 
--- src/mainPage/AddingContact.js
+++ src/mainPage/AddingContact.js
@@ -12,7 +12,7 @@ function AddingContact(props) {
     const addConatct = function (e) {
         e.preventDefault();
         let obj = Contacts.find(o => o.username == props.curUser);
-        const contact = { contactName: addBox.current.value, contactMessage: '', time: curTime.getHours() + ":" + curTime.getMinutes(), image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Netanyahu_official_portrait_%28cropped%29.jpg/250px-Netanyahu_official_portrait_%28cropped%29.jpg" }
+        const contact = { contactName: addBox.current.value, contactMessage: '', time: curTime.getHours() + ":" + curTime.getMinutes(), image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Netanyahu_official_portrait_%28cropped%29.jpg/250px-Netanyahu_official_portrait_%28cropped%29.jpg", chat: [] }
         obj.userContacts.unshift(contact)
         props.setList(obj.userContacts)
         props.setInputText(addBox.current.value)
